b11af8cf195c8376f43c1b87b1dea08ac213af94
mullma <mull_ma@163.com>
2017-12-04 17:16:57 +0800

    test git

---
+++ src/main/java/demo/HelloApplication.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET, path = "hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "helloMap")
+    public Map helloMap() {
+        Map map = new HashMap();
+        map.put("test", "123456");
+        return map;
+    }
+
+}
